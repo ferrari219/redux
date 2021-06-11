2f65d4efe1dade920bc97fe43c2148c139559861
ferrari219@nate.com <ferrari219@nate.com>
2021-06-11 15:46:08 +0900

    Done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
+//redux
+import { createStore } from 'redux';
+
 const add = document.getElementById('js_add');
 const minus = document.getElementById('js_minus');
 const number = document.querySelector('.countView');
 
 // console.log(add, minus, countView);
 
-let count = 0;
-number.innerText = count;
+const reducer = (state = 0, action) => {
+	console.log(state, action); // action: @@redux/Init
+	//리듀서는 타입을 통해 제어
+	if (action.type === 'Add') {
+		console.log('This is Add');
+		return state + 1;
+	} else if (action.type === 'Minus') {
+		console.log('This is Minus');
+		return state - 1;
+	} else {
+		return state;
+	}
+	return state;
+}; // reducer: 내 데이터 변경
 
-//function
-const handleUpdate = () => {
-	number.innerText = count;
-};
-const handleAdd = () => {
-	// console.log('Add');
-	count++;
-	handleUpdate();
-};
-const handleMinus = () => {
-	// console.log('Minus');
-	count--;
-	handleUpdate();
-};
+const store = createStore(reducer); // createStore: 내 데이터 저장소
+console.log('Store:', store);
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+//action
+store.dispatch({ type: 'Add' });
+store.dispatch({ type: 'Add' });
+store.dispatch({ type: 'Add' });
+store.dispatch({ type: 'Add' });
+store.dispatch({ type: 'Minus' });
+
+//상태확인
+console.log(store.getState());
