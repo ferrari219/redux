faa716edfaedd8fec4693cccc20bf3406e372eeb
ferrari219@nate.com <ferrari219@nate.com>
2021-06-11 14:09:17 +0900

    Vanilla Counter

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const add = document.getElementById('js_add');
+const minus = document.getElementById('js_minus');
+const number = document.querySelector('.countView');
+
+// console.log(add, minus, countView);
+
+let count = 0;
+number.innerText = count;
+
+const handleUpdate = () => {
+	number.innerText = count;
+};
+const handleAdd = () => {
+	// console.log('Add');
+	count++;
+	handleUpdate();
+};
+const handleMinus = () => {
+	// console.log('Minus');
+	count--;
+	handleUpdate();
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
