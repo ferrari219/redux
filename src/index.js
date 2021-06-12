4fb5e8150a93d0a5404091d051200d0f1e889a3e
ferrari219@nate.com <ferrari219@nate.com>
2021-06-12 11:18:43 +0900

    redux counter

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('js_add');
 const minus = document.getElementById('js_minus');
 const number = document.querySelector('.number');
 
 // console.log(add, minus, number);
+number.innerText = 0;
 
-let count = 0;
-number.innerText = count;
-
-const handleUpdate = () => {
-	number.innerText = count;
-};
-const handleAdd = () => {
-	// console.log('Add');
-	count++;
-	console.log(count);
-	handleUpdate();
-};
-const handleMinus = () => {
-	// console.log('Minus');
-	count--;
-	console.log(count);
-	handleUpdate();
+// 수정명령어 보관소
+const reducer = (state = 0, action) => {
+	console.log('reducer:', state, action);
+	if (action.type === 'Add') {
+		console.log('Add');
+		return state + 1;
+	} else if (action.type === 'Minus') {
+		console.log('Minus');
+		return state - 1;
+	} else return state;
 };
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+// 저장소
+const store = createStore(reducer);
+console.log('store:', store);
+
+// 구독
+const numberUpdate = () => {
+	number.innerText = store.getState();
+	// console.log('detect');
+};
+store.subscribe(numberUpdate);
+
+// 액션
+add.addEventListener('click', () => store.dispatch({ type: 'Add' }));
+minus.addEventListener('click', () => store.dispatch({ type: 'Minus' }));
