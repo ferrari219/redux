27da7569d8627d2397e66fcc8a71ee7113de9097
ferrari219@nate.com <ferrari219@nate.com>
2021-06-14 14:44:55 +0900

    initial

--- src/index.js
+++ src/index.js
@@ -1,32 +1,3 @@
-import { createStore } from 'redux';
-
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
-
-const reducer = (state = [], action) => {
-	if (action.type === 'Add_todo') {
-		console.log(action.text);
-		return [...state, { text: action.text, id: Date.now() }];
-	} else if (action.type === 'Del_todo') {
-		return [];
-	}
-	return state;
-};
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-// const createTodo = (todo) => {
-// 	const li = document.createElement('li');
-// 	li.innerText = todo;
-// 	ul.append(li);
-// };   Done
-const onSubmit = (e) => {
-	e.preventDefault();
-	// console.log('submit');
-	const todo = input.value;
-	input.value = '';
-	store.dispatch({ type: 'Add_todo', text: todo });
-};
-form.addEventListener('submit', onSubmit);
+const form = document.querySelector('.container form');
+const input = document.querySelector('.container input');
+const ul = document.querySelector('.container ul');
