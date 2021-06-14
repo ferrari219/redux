9dda8b69226c4137a7e0abc0a0272bd5881e5494
ferrari219@nate.com <ferrari219@nate.com>
2021-06-14 11:56:24 +0900

    초기값 설정, 버튼 셋팅

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 const number = document.querySelector('.counter .js_countView');
 const add = document.querySelector('.counter .js_add');
 const minus = document.querySelector('.counter .js_minus');
+// console.log(number, add, minus);
 
-console.log(number, add, minus);
+//변수 초기값 셋팅
+let count = 0;
+number.innerText = count;
+
+//액션 셋팅
+const handleAdd = () => {
+	console.log('Add');
+};
+const handleMinus = () => {
+	console.log('Minus');
+};
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
