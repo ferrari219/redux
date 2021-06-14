6366acca8e5038c19667b080a18a6c1939719428
ferrari219@nate.com <ferrari219@nate.com>
2021-06-14 11:53:46 +0900

    타겟팅 설정

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
-const add = document.getElementById('js_add');
-const minus = document.getElementById('js_minus');
-const number = document.querySelector('.countView');
+//타겟팅 설정
+const number = document.querySelector('.counter .js_countView');
+const add = document.querySelector('.counter .js_add');
+const minus = document.querySelector('.counter .js_minus');
 
-// console.log(add, minus, countView);
-
-let count = 0;
-number.innerText = count;
-
-//function
-const handleUpdate = () => {
-	number.innerText = count;
-};
-const handleAdd = () => {
-	// console.log('Add');
-	count++;
-	handleUpdate();
-};
-const handleMinus = () => {
-	// console.log('Minus');
-	count--;
-	handleUpdate();
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(number, add, minus);
