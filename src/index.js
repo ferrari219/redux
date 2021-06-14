925472cb76e5d2e9d34da970923bccf7081e0454
ferrari219@nate.com <ferrari219@nate.com>
2021-06-14 13:08:47 +0900

    Done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { createStore } from 'redux';
 
 //타겟팅 설정
-const number = document.querySelector('.counter .js_countView');
-const add = document.querySelector('.counter .js_add');
-const minus = document.querySelector('.counter .js_minus');
+const number = document.querySelector('.counter > .js_countView');
+const add = document.querySelector('.counter > .js_add');
+const minus = document.querySelector('.counter > .js_minus');
 // console.log(number, add, minus);
 
 //변수 초기값 셋팅
