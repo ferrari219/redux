9376de92c0bce191469d4ab40ce5361c2362d012
ferrari219@nate.com <ferrari219@nate.com>
2021-06-14 13:17:16 +0900

    Done

--- src/index.js
+++ src/index.js
@@ -1,44 +1,28 @@
-import { createStore } from 'redux';
-
 //타겟팅 설정
-const number = document.querySelector('.counter > .js_countView');
-const add = document.querySelector('.counter > .js_add');
-const minus = document.querySelector('.counter > .js_minus');
+const number = document.querySelector('.counter .js_countView');
+const add = document.querySelector('.counter .js_add');
+const minus = document.querySelector('.counter .js_minus');
 // console.log(number, add, minus);
 
 //변수 초기값 셋팅
 let count = 0;
 number.innerText = count;
 
-// reducer
-const reducer = (state = 0, action) => {
-	// console.log(state, action);
-	switch (action.type) {
-		case 'Add':
-			return state + 1;
-		case 'Minus':
-			return state - 1;
-		default:
-			return state;
-	}
+//액션 셋팅
+const handleUpdate = (count) => {
+	number.innerText = count;
 };
-
-// store
-const store = createStore(reducer);
-
-//subscribe
-const onChange = () => {
-	number.innerText = store.getState();
-};
-store.subscribe(onChange);
-
-// action
 const handleAdd = () => {
-	store.dispatch({ type: 'Add' });
+	// console.log('Add');
+	count++;
+	console.log(count);
+	handleUpdate(count);
 };
 const handleMinus = () => {
-	store.dispatch({ type: 'Minus' });
+	// console.log('Minus');
+	count--;
+	console.log(count);
+	handleUpdate(count);
 };
-
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
