7146cc3823ea0034f0fa1b5c675ebacd11360900
ferrari219@nate.com <ferrari219@nate.com>
2021-06-15 16:08:56 +0900

    todolist 제작

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+	const [text, setText] = useState('');
+	const onChange = (e) => {
+		const value = e.target.value;
+		setText(value);
+	};
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log(text);
+		setText('');
+	};
 	return (
 		<div>
 			<h1>To do</h1>
-			<input type="text" />
+			<form onSubmit={onSubmit}>
+				<input type="text" value={text} onChange={onChange} />
+				<button>Add</button>
+			</form>
 		</div>
 	);
 };
