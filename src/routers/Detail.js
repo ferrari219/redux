dbfe084c9a3988254fefb74f470a05de38b187fc
ferrari219@nate.com <ferrari219@nate.com>
2021-06-18 10:56:37 +0900

    Done

--- src/routers/Detail.js
+++ src/routers/Detail.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Detail = () => {
-	return <div></div>;
+const Detail = (props) => {
+	console.log('Detail: ', props);
+	return (
+		<div>
+			<h1>{props.detail.text}</h1>
+		</div>
+	);
 };
 
-export default Detail;
+const mapStateToProps = (state, ownProps) => {
+	console.log('Detail mapStateToProps :', state, ownProps);
+	return {
+		detail: state.find((item) => item.id === parseInt(ownProps.match.params.id)),
+	};
+};
+
+export default connect(mapStateToProps)(Detail);
